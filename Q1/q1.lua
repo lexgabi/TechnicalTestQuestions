8ad70b7c60de527af31d629a71a9e5d5903e034d
lexgabi <alexis.j.gabiola@gmail.com>
2024-05-12 16:20:19 -0400

    Edit made to original Q1 question

--- Q1/q1.lua
+++ Q1/q1.lua
@@ -3,14 +3,14 @@
 
 -- Fix or improve the implementation of the below methods
 
-
-local function releaseStorage(player)
-    player:setStorageValue(1000, -1)
+-- This function takes a player and their ID, and resets the storage value to -1
+local function releaseStorage(player, storageID)
+    player:setStorageValue(storageID, -1)
 end
     
-
-function onLogout(player)
-    if player:getStorageValue(1000) == 1 then addEvent(releaseStorage, 1000, player)
+-- This function tests if a player does not have an empty storage when logging off
+function onLogout(player, storageID)
+    if player:getStorageValue(storageID) >= 0 then addEvent(releaseStorage, storageID, player)
     end
     return true
 end
