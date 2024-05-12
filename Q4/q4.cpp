a68b03844b86dbcfde8787c9f68011c52ec7a6c3
lexgabi <alexis.j.gabiola@gmail.com>
2024-05-11 21:44:10 -0400

    Edits made to original Q4

--- Q4/q4.cpp
+++ Q4/q4.cpp
@@ -3,25 +3,42 @@
 
 // Assume all method calls work fine. Fix the memory leak issue in below method
 
+#include <string>
+#include <iostream>
 
-void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
+using namespace std;
+
+// This function is intended to add an Item class object to a Player class object
+void Game::addItemToPlayer(const string& recipient, uint16_t itemId)
 {
-Player* player = g_game.getPlayerByName(recipient);
-if (!player) {
-player = new Player(nullptr);
-if (!IOLoginData::loadPlayerByName(player, recipient)) {
-return;
-}
-}
+    // Class pointer to address of object of the Player class
+    Player* player = g_game.getPlayerByName(recipient);
 
-Item* item = Item::CreateItem(itemId);
-if (!item) {
-return;
-}
+    // Check to see if player's memory address is non-null
+    if (!player) {
+        player = new Player(nullptr);
+        if (!IOLoginData::loadPlayerByName(player, recipient)) {
+            return;
+        }
+    }
+    
+    // Class pointer to address of object of the Item class
+    Item* item = Item::CreateItem(itemId);
+    
+    // Check to see if item's memory address is non-null
+    if (!item) {
+        return;    
+    }
+        
+    // Add item to player's internal inbox at any index
+    g_game.internalAddItem(player->getInbox(), item, INDEX_WHEREEVER, FLAG_NOLIMIT);
 
-g_game.internalAddItem(player->getInbox(), item, INDEX_WHEREEVER, FLAG_NOLIMIT);
+    // If player's status is offline, then save player's progress
+    if (player->isOffline()) {
+        IOLoginData::savePlayer(player);
+     }
 
-if (player->isOffline()) {
-IOLoginData::savePlayer(player);
-}
+    // Frees up memory allocated by 'new' call
+     delete player;
+
 }
